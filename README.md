4d1ad3af2185302b8398b8c3d4a6bf324cc87521
abhinavs1729 <87321901+abhinavs1729@users.noreply.github.com>
2023-09-08 09:39:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Image-search-engine
+ HTML + CSS + JS app that searches images for your input
---
+++ script.js
@@ -0,0 +1,5 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchresult = document.getElementById("search-result");
+const showmore = document.getElementById("Show-more");
+
